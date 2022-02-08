1b0c2a9986ce6c65dbb1264acd0dd609e9eea2b6
IlhamAyZa <IZADA20@KU.EDU.TR>
2022-02-08 13:44:10 +0400

    Update dinArrayProblem.c

--- dinArrayProblem.c
+++ dinArrayProblem.c
@@ -25,12 +25,14 @@ int main(int argc, char const* argv[])
 	*/
 
 	//2 
-
+	/*
 	double* arrOfFrx;
 	int num, num1, num2;
 
 	printf("Enter amount of numbers in massive: ");
 	scanf("%d", &num);
+
+	arrOfFrx = (double*)malloc(num * sizeof(double));
 
 	for (int i = 0; i < num; i++)
 	{
@@ -41,7 +43,17 @@ int main(int argc, char const* argv[])
 
 	}
 
+	for (int i = 0; i < num; i++)
+	{
+		printf("%.3f ", arrOfFrx[i]);
+
+	}
+
+	printf("\n\n");
+
 	num1 = 2 * num;
+
+	arrOfFrx = (double*)realloc(arrOfFrx, num1 * sizeof(double));
 
 	for (int i = num; i < num1; i++)
 	{
@@ -51,7 +63,48 @@ int main(int argc, char const* argv[])
 	for (int i = 0; i < num1; i++)
 	{
 		printf("%.3f ", arrOfFrx[i]);
+
+	}
+	*/
+
+	//3 
+
+	float* vector1;
+	float* vector2;
+	int num1, num2;
+
+	printf("Enter amount of elements in vector #1 : ");
+	scanf("%d", &num1);
+
+	vector1 = (float*)malloc(num1 * sizeof(double));
+
+	for (int i = 0; i < num1; i++)
+	{
+		printf(": ");
+		scanf("%f", &vector1[i]);
 	}
 
+	printf("Enter amount of elements in vector #2 : ");
+	scanf("%d", &num2);
+
+	vector2 = (float*)malloc(num2 * sizeof(double));
+
+	for (int i = 0; i < num2; i++)
+	{
+		printf(": ");
+		scanf("%f", &vector2[i]);
+	}
+
+	vector1 = realloc(vector1, (num1 + num2) * sizeof(float));
+
+	for (int i = num1; i < num2 + num1; i++)
+	{
+		vector1[i] = vector2[i - num1];
+	}
+
+	for (int i = 0; i < num1 + num2; i++)
+	{
+		printf("%.3f ", vector1[i]);
+	}
 	return 0;
 }
